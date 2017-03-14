f2ce3cf613430386eae933793e059d14b9ec76b2
Lealcy Tardelli Belegante <lealcy@gmail.com>
2017-03-14 17:01:59 -0300

    Added a function to check if two lines intersect in a plane.

--- src/MathToolbox.js
+++ src/MathToolbox.js
@@ -6,7 +6,7 @@ MathToolbox.lerp = function(v1, v2, t) {
 };
 
 // Map a value in one range to another.
-MathToolbox.map = function (value, r1Min, r1Max, r2Min, r2Max) {
+MathToolbox.map = function(value, r1Min, r1Max, r2Min, r2Max) {
     return (value - r1Min) * (r2Max - r2Min) / (r1Max - r1Min) + r2Min;
 };
 
@@ -21,6 +21,18 @@ MathToolbox.dist = function(x1, y1, x2, y2) {
 };
 
 // Return the midpoint between two points in a plane
-MathToolbox.midpoint = function (x1, y1, x2, y2) {
+MathToolbox.midpoint = function(x1, y1, x2, y2) {
     return { x: (x1 + x2) / 2, y: (y1 + y2) / 2 };
+}
+
+// Check if two lines intersect
+mathtoolbox.doLinesIntersect = function(x1, y1, x2, y2, x3, y3, x4, y4) {
+    if (x1 == x2) {
+        return !(x3 == x4 && x1 != x3);
+    } 
+    if (x3 == x4) {
+        return true;
+    } 
+    // Both lines are not parallel to the y-axis
+    return (y1-y2)/(x1-x2) !== (y3-y4)/(x3-x4);
 }
