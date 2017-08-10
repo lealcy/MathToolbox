1ac313bcca025c3f4193ac4a8dfe3ff2c1dac0ae
Lealcy Tardelli Belegante <lealcy@gmail.com>
2017-08-10 14:40:01 -0300

    Math.formatNumber()

--- src/MathToolbox.js
+++ src/MathToolbox.js
@@ -67,3 +67,16 @@ Math.getRandomInt = Math.getRandomInt || function(min, max) {
 Math.deg2rad = Math.deg2rad || function(degrees) {
     return degrees * (Math.PI / 180);
 };
+
+// Format number
+Math.formatNumber = Math.formatNumber || function(num, digits) {
+    let units = ['k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'];
+    let decimal;
+    for (let i = units.length - 1; i >= 0; i--) {
+        decimal = Math.pow(1000, i + 1);
+        if (num <= -decimal || num >= decimal) {
+            return +(num / decimal).toFixed(digits) + units[i];
+        }
+    }
+    return num;
+}
